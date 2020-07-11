9298d58e10f9e1ef5e0f3491403aecfd4fcb5d0f
Marcus Thelin <marcus.thelin@24hr.se>
2020-07-11 23:11:04 +0200

    Refactor and better error handling and logging

--- index.js
+++ index.js
@@ -2,73 +2,40 @@
 
 const path = require('path');
 const fs = require('fs');
-const { spawn } = require('child_process');
-const yargs = require('yargs');
+
+const { logger, readJsonFile, argumentParser, buildCommander, validateJson } = require('./lib');
+
 // const blockPlugins = require('./block-plugins.json');
 
-const cmds = [];
+global.spawnedCommands = [];
 
-function getJson() {
-    const argv = yargs
-        .option('f', {
-            alias: 'file',
-            demandOption: true,
-            describe: 'The file path to json file.',
-            type: 'string',
-        })
-        .option('p', {
-            alias: 'plugin-folder',
-            default: './plugins',
-            describe: 'The path to your plugins folder',
-            type: 'string',
-        }).argv;
-    const filePath = argv.f;
-    const jsonFileExists = fs.existsSync(filePath);
-    if (!jsonFileExists) {
-        logger('JSON file does not exist!');
-        process.exit(1);
-    }
-    const jsonFileContent = fs.readFileSync(filePath, 'utf-8');
+process.on('SIGTERM', () => {
+    global.spawnedCommands.forEach(command => command.kill());
+});
+
+function countSuccessfulCommands(result) {
+    return result.reduce((acc, curr) => {
+        if (curr.status === 'fulfilled') {
+            return ++acc;
+        }
+        return acc;
+    }, 0);
+}
+
+async function start() {
     try {
-        const json = JSON.parse(jsonFileContent);
-        run(json, argv.p);
-    } catch (error) {
-        logger(error);
-        process.exit(1);
+        const { f: jsonFilePath, p: pluginsPath } = argumentParser();
+        const jsonFileContent = readJsonFile(jsonFilePath); // Syncronous
+        validateJson(jsonFileContent); // Either throws or let us continue the program
+        const result = await buildCommander(jsonFileContent, pluginsPath);
+        const successFulCommands = countSuccessfulCommands(result);
+        logger(`Successfully built ${successFulCommands}/${jsonFileContent.length} plugins`);
+        if (successFulCommands !== jsonFileContent.length) {
+            logger('One or more plugin(s) could not be built. Please see error log(s).');
+        }
+    } catch (err) {
+        logger(err);
     }
 }
-getJson();
 
-function logger(str) {
-    process.stdout.write('\n' + str);
-}
-
-async function builder(path) {
-    return new Promise((resolve, reject) => {
-        if (!fs.existsSync(path)) {
-            logger(`Path ${path} does not exist!`);
-            return;
-        }
-        const cmd = spawn('npm', [`--prefix ${path} run build`], {
-            stdio: [process.stdin, process.stdout, process.stderr],
-            shell: true,
-        });
-        cmds.push(cmd);
-        cmd.once('error', reject);
-        cmd.once('exit', resolve);
-    });
-}
-
-async function run(blockPlugins, pluginsPath) {
-    if (Array.isArray(blockPlugins) && blockPlugins.length > 0) {
-        for (const plugin of blockPlugins) {
-            const pluginPath = path.resolve(pluginsPath, plugin);
-            logger(`Installing ${plugin}`);
-            await builder(pluginPath).catch(err => {
-                logger(`Failed to install ${plugin}. ${err}`);
-            });
-        }
-        logger('Done!');
-        process.exit(0);
-    }
-}
+start();
